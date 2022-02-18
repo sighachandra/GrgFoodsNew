b008d9f9d96e914edd9241c16ff2f037671f36be
sighachandra <sighac@gmail.com>
2022-02-18 17:44:38 +0530

    sidebar fixed

--- assetsgr/js/script.js
+++ assetsgr/js/script.js
@@ -1,1 +1,20 @@
-njxnjkkm
+// function leftSideDropdownone() {
+//   document.getElementById("leftDropdownone").classList.toggle("show_l");
+// }
+// function leftSideDropdowntwo() {
+//   document.getElementById("leftDropdowntwo").classList.toggle("show_l");
+// }
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn_l')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content_l");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show_l')) {
+//         openDropdown.classList.remove('show_l');
+//       }
+//     }
+//   }
+// }
+
+
