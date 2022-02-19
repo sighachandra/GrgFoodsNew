7217142a12dda4af2bfe44a3515cc604bde51a62
sighachandra <sighac@gmail.com>
2022-02-19 16:09:47 +0530

    header and images fixed

--- assetsgr/js/script.js
+++ assetsgr/js/script.js
@@ -18,3 +18,22 @@
 // }
 
 
+//         window.onscroll = function() {myFunction()};
+
+// var header = document.getElementById("myHeader");
+// var sticky = header.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
+    
+ window.addEventListener("scroll", function() {
+        var mainH = document.getElementById("myHeader");
+       
+        mainH.classList.toggle("sticky", window.scrollY > mainH.scrollHeight);
+        
+      });
