1ee2e93799a73e8c9d2dfdbe40173fa947ee322e
sighachandra <sighac@gmail.com>
2022-02-22 15:21:50 +0530

    filters modal fixed

--- assetsgr/js/script.js
+++ assetsgr/js/script.js
@@ -60,3 +60,30 @@ option.forEach(a => {
               parent.innerText = b.target.innerText;
        })
 })
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close_filter")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
